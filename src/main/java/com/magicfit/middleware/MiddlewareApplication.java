bc00f4c36b1a4feadbbe3f0f5dbf0bf872ce4b74
spawn08 <amarthkr66@gmail.com>
2019-06-29 02:15:55 +0530

    Changing the base package of application main class

--- src/main/java/com/magicfit/middleware/MiddlewareApplication.java
+++ src/main/java/com/magicfit/middleware/MiddlewareApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(scanBasePackages = "com.spawnai")
+@SpringBootApplication(scanBasePackages = "com.magicfit")
 public class MiddlewareApplication {
 
     public static void main(String[] args) {
